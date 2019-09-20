cf239c801cafef3b759baa8a1160097c9d73f40b
Tim Morello <tdmorello@gmail.com>
2019-09-20 18:04:55 -0400

    Create get_random_content.py

---
+++ get_random_content.py
@@ -0,0 +1,5 @@
+import requests
+
+requests.get('https://www.randomlists.com/data/words.json')
+requests.get('https://www.randomlists.com/data/things.json')
+requests.get('https://www.randomlists.com/data/nouns.json')
