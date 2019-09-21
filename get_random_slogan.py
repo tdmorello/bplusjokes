25e4b5182ab929550cece32f588af854c6f6a19e
Tim Morello <tdmorello@gmail.com>
2019-09-20 20:53:59 -0400

    Add function to get random business slogans

--- get_random_slogan.py
+++ get_random_slogan.py
@@ -1,26 +1,22 @@
 import mechanicalsoup
 from bs4 import BeautifulSoup
 
-search_term = 'kitty'
+def get_random_slogan(search_term):
+    """Returns a list of slogans from a given term using shopify's slogan maker"""
+    
+    # Submit form on website
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open('https://www.shopify.com/tools/slogan-maker')
+    browser.select_form('form[action="/tools/slogan-maker/create"]')
+    browser['term'] = search_term
+    response = browser.submit_selected()
 
-browser = mechanicalsoup.StatefulBrowser()
-
-browser.open('https://www.shopify.com/tools/slogan-maker')
-
-browser.select_form('form[action="/tools/slogan-maker/create"]')
-
-browser['term'] = search_term
-
-response = browser.submit_selected()
-
-raw_html = response.text
-
-soup = BeautifulSoup(raw_html, 'html.parser') 
-
-content = soup.find('div', class_='content')
-
-slogans = []
-for slogan in content.findAll('button'):
-    slogans.append(slogan.text.strip())
-
-print(len(slogan))
+    # Parse response
+    raw_html = response.text
+    soup = BeautifulSoup(raw_html, 'html.parser') 
+    content = soup.find('div', class_='grid-container grid-container--halves grid--striped')
+    slogans = []
+    for slogan in content.findAll('button'):
+        slogans.append(slogan.text.strip())
+    
+    return slogans
