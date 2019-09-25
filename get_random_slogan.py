a078109baf29fb43ee1fb3418b5a44194cc7bf64
Tim Morello <tdmorello@gmail.com>
2019-09-25 14:05:01 -0400

    Add re-attempt to connect to slogan maker if failed

--- get_random_slogan.py
+++ get_random_slogan.py
@@ -2,13 +2,34 @@ import mechanicalsoup
 from bs4 import BeautifulSoup
 from get_random_content import get_random_nouns, get_random_words, get_random_things
 import random
+import requests
+import logging
+import time
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+
+
+def check_connection(url):
+    good_connection = False
+    while good_connection == False:
+        try:
+            requests.get(url)
+        except requests.exceptions.ConnectionError as e:
+            logging.error(f'Cannot connect to {url}: {e}. Trying again in 5s.')
+            time.sleep(5)
+            pass
+    return None
 
 def get_random_slogan(search_term):
     """Returns a list of slogans from a given term using shopify's slogan maker"""
     
+    url = 'https://www.shopify.com/tools/slogan-maker'
+    
+    check_connection(url)
+    
     # Submit form on website
     browser = mechanicalsoup.StatefulBrowser(soup_config={'features': 'html.parser'})
-    browser.open('https://www.shopify.com/tools/slogan-maker')
+    browser.open(url)
     browser.select_form('form[action="/tools/slogan-maker/create"]')
     browser['term'] = search_term
     response = browser.submit_selected()
